3cf1659c84a7054ad277b54cc325bd38bc6c1426
Xowie89 <41758045+Xowie89@users.noreply.github.com>
2024-12-12 18:01:50 -0500

    Update creepRoles.js

--- creepRoles.js
+++ creepRoles.js
@@ -6,78 +6,92 @@ module.exports = {
 
         switch (creep.memory.role) {
             case 'harvester':
+                // Harvester logic
                 target = creep.room.find(FIND_SOURCES)[0];
                 if (target) pathfindingManager.moveCreep(creep, target.pos);
                 break;
 
             case 'upgrader':
-                target = creep.room.controller;
-                if (target) pathfindingManager.moveCreep(creep, target.pos);
+                if (creep.store[RESOURCE_ENERGY] === 0) {
+                    // Fetch energy
+                    target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                        filter: s => s.structureType === STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] > 0,
+                    });
+                    if (!target) target = creep.pos.findClosestByPath(FIND_SOURCES);
+                    if (target) {
+                        if (creep.withdraw(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE || creep.harvest(target) === ERR_NOT_IN_RANGE) {
+                            pathfindingManager.moveCreep(creep, target.pos);
+                        }
+                    }
+                } else if (creep.room.controller) {
+                    // Upgrade controller
+                    if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
+                        pathfindingManager.moveCreep(creep, creep.room.controller.pos);
+                    }
+                } else {
+                    // Idle behavior: Help build or repair
+                    this.handleIdleBehavior(creep);
+                }
                 break;
 
             case 'builder':
                 target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-                if (target) pathfindingManager.moveCreep(creep, target.pos);
+                if (target) {
+                    if (creep.build(target) === ERR_NOT_IN_RANGE) {
+                        pathfindingManager.moveCreep(creep, target.pos);
+                    }
+                } else {
+                    // Idle behavior: Upgrade controller
+                    this.handleIdleBehavior(creep);
+                }
                 break;
 
             case 'repairer':
                 target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                     filter: structure => structure.hits < structure.hitsMax,
                 });
-                if (target) pathfindingManager.moveCreep(creep, target.pos);
-                break;
-
-            case 'carrier':
-                if (creep.store.getFreeCapacity() > 0) {
-                    target = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
-                        filter: resource => resource.resourceType === RESOURCE_ENERGY,
-                    });
-                    if (target) pathfindingManager.moveCreep(creep, target.pos);
-                } else {
-                    target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                        filter: structure =>
-                            (structure.structureType === STRUCTURE_SPAWN ||
-                                structure.structureType === STRUCTURE_EXTENSION ||
-                                structure.structureType === STRUCTURE_STORAGE) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0,
-                    });
-                    if (target) pathfindingManager.moveCreep(creep, target.pos);
-                }
-                break;
-
-            case 'scout':
-                if (!creep.memory.targetRoom) {
-                    const unexploredRooms = Object.keys(Game.map.describeExits(creep.room.name)).filter(
-                        exit => !Memory.exploredRooms || !Memory.exploredRooms[exit]
-                    );
-                    if (unexploredRooms.length > 0) {
-                        creep.memory.targetRoom = unexploredRooms[0];
-                    }
-                }
-                if (creep.memory.targetRoom && creep.room.name !== creep.memory.targetRoom) {
-                    const exitDir = creep.room.findExitTo(creep.memory.targetRoom);
-                    const exit = creep.pos.findClosestByRange(exitDir);
-                    if (exit) creep.moveTo(exit);
-                } else {
-                    Memory.exploredRooms = Memory.exploredRooms || {};
-                    Memory.exploredRooms[creep.room.name] = true;
-                }
-                break;
-
-            case 'defender':
-                target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
                 if (target) {
-                    if (creep.attack(target) === ERR_NOT_IN_RANGE) {
+                    if (creep.repair(target) === ERR_NOT_IN_RANGE) {
                         pathfindingManager.moveCreep(creep, target.pos);
                     }
                 } else {
-                    const spawn = creep.room.find(FIND_MY_SPAWNS)[0];
-                    if (spawn) creep.moveTo(spawn);
+                    // Idle behavior: Help harvest energy
+                    this.handleIdleBehavior(creep);
                 }
                 break;
 
-            case 'remoteMiner':
-                target = Game.getObjectById(creep.memory.targetSourceId);
+            // Additional roles like carrier, scout, etc.
+        }
+    },
+
+    handleIdleBehavior: function (creep) {
+        let target;
+
+        switch (creep.memory.role) {
+            case 'upgrader':
+            case 'repairer':
+                // Idle upgraders/repairers help with building
+                target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+                if (target) {
+                    if (creep.build(target) === ERR_NOT_IN_RANGE) {
+                        pathfindingManager.moveCreep(creep, target.pos);
+                    }
+                }
+                break;
+
+            case 'builder':
+                // Idle builders help upgrade controller
+                target = creep.room.controller;
+                if (target) {
+                    if (creep.upgradeController(target) === ERR_NOT_IN_RANGE) {
+                        pathfindingManager.moveCreep(creep, target.pos);
+                    }
+                }
+                break;
+
+            case 'repairer':
+                // Idle repairers harvest energy if no repairs are needed
+                target = creep.pos.findClosestByPath(FIND_SOURCES);
                 if (target) {
                     if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
                         pathfindingManager.moveCreep(creep, target.pos);
@@ -85,32 +99,8 @@ module.exports = {
                 }
                 break;
 
-            case 'remoteCarrier':
-                if (creep.store.getFreeCapacity() > 0) {
-                    target = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
-                    if (target) pathfindingManager.moveCreep(creep, target.pos);
-                } else {
-                    target = creep.room.storage;
-                    if (target && creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                        pathfindingManager.moveCreep(creep, target.pos);
-                    }
-                }
-                break;
-
-            case 'colonizer':
-                if (creep.memory.targetRoom && creep.room.name !== creep.memory.targetRoom) {
-                    const exitDir = creep.room.findExitTo(creep.memory.targetRoom);
-                    const exit = creep.pos.findClosestByRange(exitDir);
-                    if (exit) creep.moveTo(exit);
-                } else {
-                    target = creep.room.controller;
-                    if (target && creep.claimController(target) === ERR_NOT_IN_RANGE) {
-                        pathfindingManager.moveCreep(creep, target.pos);
-                    }
-                }
-                break;
-
-            // Add additional roles as needed
+            default:
+                console.log(`${creep.name} is idle with no productive task.`);
         }
-    }
+    },
 };
