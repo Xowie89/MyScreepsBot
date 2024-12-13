5b9f2816a54498414df68dc7b0f22baea51768b8
Xowie89 <41758045+Xowie89@users.noreply.github.com>
2024-12-13 04:37:45 -0500

    Create upgrader.js

---
+++ upgrader.js
@@ -0,0 +1,15 @@
+// upgrader.js
+module.exports = {
+    run: function (creep) {
+        if (creep.store[RESOURCE_ENERGY] === 0) {
+            const source = creep.pos.findClosestByPath(FIND_SOURCES);
+            if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+            }
+        } else {
+            if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+        }
+    }
+};
